70961453e29922f03763cd066c1b99406ebdf067
Kayki Santana Guimarães <kaykisan2006@gmail.com>
2026-08-23 17:48:20 -0300

    Configura variáveis de ambiente

--- model/repository/conexao.php
+++ model/repository/conexao.php
@@ -1,10 +1,15 @@
 <?php
 
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
+$dotenv->load();
+
 $conexao = mysqli_connect(
-    "localhost",
-    "root",
-    "branquinha",
-    "inventario"
+    $_ENV["DB_HOST"],
+    $_ENV["DB_USER"],
+    $_ENV["DB_PASSWORD"],
+    $_ENV["DB_NAME"]
 );
 
 if (!$conexao) {
