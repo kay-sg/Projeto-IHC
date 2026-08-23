7e62bebf8c4d8dc5a20924f2996e756093a2e335
Grobie <grubyersus@gmail.com>
2026-08-23 20:10:26 -0300

    falta terminar template, manual e automatico

--- model/Minijogo.java
+++ model/Minijogo.java
@@ -1,3 +1,35 @@
 public class Minijogo {
 
+    private int idMinijogo;
+    private String nome, descricao;
+    private float recorde, pontuacao;
+
+    public Minijogo(int id, String n, String d, float rc, float pt){
+        this.idMinijogo = id;
+        this.nome = n;
+        this.descricao = d;
+        this.recorde = rc;
+        this.pontuacao = pt;
+    }
+
+    public int getIdMinijogo() {
+        return this.idMinijogo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public float getRecorde() {
+        return this.recorde;
+    }
+
+    public float getPontuacao() {
+        return this.pontuacao;
+    }
+
 }
