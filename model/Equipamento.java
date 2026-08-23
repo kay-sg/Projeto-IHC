db4e6d18600e022f921b0fbb1090095bc757d0d1
Grobie <grubyersus@gmail.com>
2026-08-22 23:12:00 -0300

    preenchendo as classes jogador e equipamento

--- model/Equipamento.java
+++ model/Equipamento.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
 public class Equipamento {
 
+    private int idEquipamento;
+    private String nome;
+    private float qualidade;
+    private boolean metodoCrafting;
+    private int tipo;
+    private ArrayList<Float> atributos = new ArrayList<>();
+
+    public Equipamento(int id, String n, float qd, boolean mc, int tp){
+        this.idEquipamento = id;
+        this.nome = n;
+        this.qualidade = qd;
+        this.metodoCrafting = mc;
+        this.tipo = tp;
+    }
+
+    public void atribuicao(float at){
+        atributos.add(at);
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 }
--- model/Jogador.java
+++ model/Jogador.java
@@ -1,3 +1,21 @@
 public class Jogador {
 
+    private int idJogador;
+    private int nivel = 0;
+
+    public Jogador(int id){
+        this.idJogador = id;
+    }
+
+    public int getIdJogador(){
+        return idJogador;
+    }
+
+    public void updateNivel(int n){
+        this.nivel += n;
+    }
+
+    public int getNivel(){
+        return this.nivel;
+    }
 }
