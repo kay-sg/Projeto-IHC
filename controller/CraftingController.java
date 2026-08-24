eaeb9a2b314b310b30cbb5c4379095b869acfc49
Grobie <grubyersus@gmail.com>
2026-08-24 03:17:37 -0300

    adicionado driver java-mysql na lib

--- controller/CraftingController.java
+++ controller/CraftingController.java
@@ -1,6 +1,7 @@
+import repository.ConexaoJava;
 public class CraftingController {
 
     public static void main(String[] args) {
-        
+        ConexaoJava conection = new ConexaoJava();
     }
 }
---
+++ model/repository/ConexaoJava.java
@@ -0,0 +1,30 @@
+package repository;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConexaoJava {
+
+    private static final String HOST = System.getenv("DB_HOST");
+    private static final String BANCO = System.getenv("DB_NAME");
+    private static final String USUARIO = System.getenv("DB_USER");
+    private static final String SENHA = System.getenv("DB_PASSWORD");
+    private static final String URL = "jdbc:mysql://" + HOST + "/" + BANCO + "?useSSL=false&serverTimezone=UTC";
+
+    private Connection con;
+    private String driver = "com.mysql.cj.jdbc.Driver";
+
+    public Connection conectar(){
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(URL,USUARIO,SENHA);
+            return con;
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+            return null;
+        }
+    }
+}
